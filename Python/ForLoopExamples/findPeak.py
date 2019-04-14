a4936e3d80da8d248f96e5b0571a2e829402aa76
Amrita Mathuriya <amrita.mathuria@gmail.com>
2019-04-14 16:53:23 -0700

    Fix.

--- Python/ForLoopExamples/findPeak.py
+++ Python/ForLoopExamples/findPeak.py
@@ -9,16 +9,16 @@ for i in range(len(list1)):
         c = list1[i]
         n = list1[i+1]
         if c >  p and c > n:
-            print (p, c, n)
+            print ("peak at: ", c, " with ", p, c, n)
     elif i == len1-1:
         p = list1[i-1]
         c = list1[i]
-        if c >  p : 
-            print (p, c )
+        if c >  p :
+            print ("peak at: ", c, " with ", p, c)
 
     elif i == 0:
         c = list1[i]
         n = list1[i+1]
         if c > n : 
-            print (c, n )
+            print ("peak at: ", c, " with ", c, n)
 
