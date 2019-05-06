6de51765dcaf40c3f1deb31ef470d776f7a26059
Amrita Mathuriya <amrita.mathuria@gmail.com>
2019-05-05 18:32:45 -0700

    Adding basic nested for loop examples.

---
+++ Python/NestedLoops/basicNested.py
@@ -0,0 +1,11 @@
+
+N = 5
+M = 4
+
+print "Outer loop dimension: ", N 
+print "Inner loop dimension: ", M
+
+for i in range(N):
+    for j in range (M):
+        print ( i, j )
+    print ""
---
+++ Python/NestedLoops/triangleNested.py
@@ -0,0 +1,9 @@
+N = 5
+
+print "Outer loop dimension: ", N 
+
+for i in range(N):
+    list1 = []
+    for j in range (i+1):
+        list1.append(j)
+    print list1 
