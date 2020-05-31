eb7b3840edfe86b18d28268dbbcb9fe8e74c46fd
Amrita Mathuriya <amrita.mathuria@gmail.com>
2020-05-30 18:08:22 -0700

    Update

--- Python/Recurssion/Fibnoccci/fibnoacci.py
+++ Python/Recurssion/Fibnoccci/fibnoacci.py
@@ -1,32 +1,40 @@
-fIn = open("input.txt", "r")
-fOut = open ("output.txt", "w")
 
-numTests = int(fIn.readline())
-print (numTests)
 
 def fun_fib(N, list1):
-    if ( N == 2 or N == 1 ):
+    if ( N <= 2 ):
         return 1, list1
 
     fib_N_1, list1 = fun_fib(N-1, list1) 
-    fib_N = list1[len(list1)-2] + fib_N_1
+    fib_N_2 = list1[len(list1)-2]
+
+    fib_N = fib_N_2 + fib_N_1
     
     list1.append(fib_N)
     return fib_N, list1 
 
-for i in range(numTests):
-    N = int( fIn.readline() )
-    list1 = [1, 1]
-    fib_N, list1 = fun_fib(N, list1)
-    fOut.write(str(list1))
-    fOut.write("\n")
-    print(list1)
+def main():
+    fIn = open("input.txt", "r")
+    fOut = open ("output.txt", "w")
+    
+    numTests = int(fIn.readline())
+    print (numTests)
 
-fIn.close()
-fOut.close()
+    for i in range(numTests):
+        N = int( fIn.readline() )
+        print (N)
+
+        list1 = [1, 1]
+        fib_N, list1 = fun_fib(N, list1)
 
 
+        fOut.write( str(list1) + "\n" )
 
-#N = 2
-#print (fun_fib(N))
+        #print(list1)
+
+    fIn.close()
+    fOut.close()
+
+
+main()
+
 
