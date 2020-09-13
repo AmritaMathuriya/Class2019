defc456135baedac3f911dfef3371fe75e579a0b
Amrita Mathuriya <amrita.mathuria@gmail.com>
2020-09-12 18:07:54 -0700

    Calculator step4

--- Python/simpleProbs/Calculator/step4.py
+++ Python/simpleProbs/Calculator/step4.py
@@ -11,30 +11,31 @@ def mult ( a, b ):
 def div ( a, b ):
     return a / b
 
-while ( True ):
-    a = input ("Feed the first number: ")
-    b = input ("Feed the second number: ")
-    a = int(a)
-    b = int(b)
+def main ()
+    while ( True ):
+        a = input ("Feed the first number: ")
+        b = input ("Feed the second number: ")
+        a = int(a)
+        b = int(b)
+        
+        ans = -1
+        
+        option = input("Please enter add/sub/mult/div: ")
+        if option == "add": 
+            ans = add (a,b)
+        
+        if option == "sub":
+            ans = sub(a,b)
+        
+        if option == "mult":
+            ans = mult(a,b)
+        
+        if option == "div":
+            ans = div(a,b)    
+        
+        print ("Answer is: ", ans)
+        TryAgain = input("Try again yes/no? : ")
+        if TryAgain == "no" : 
+            break
     
-    ans = -1
-    
-    option = input("Please enter add/sub/mult/div: ")
-    if option == "add": 
-        ans = add (a,b)
-    
-    if option == "sub":
-        ans = sub(a,b)
-    
-    if option == "mult":
-        ans = mult(a,b)
-    
-    if option == "div":
-        ans = div(a,b)    
-    
-    print ("Answer is: ", ans)
-    TryAgain = input("Try again yes/no? : ")
-    if TryAgain == "no" : 
-        break
-
-    
+        
