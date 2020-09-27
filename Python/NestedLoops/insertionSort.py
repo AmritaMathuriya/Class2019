70aaf9c473a7a5795e1e588f2f780ba89b6369f3
Amrita Mathuriya <amrita.mathuria@gmail.com>
2020-09-26 17:53:14 -0700

    Updating insertion sort previous program to look similar to recursive one.

--- Python/NestedLoops/insertionSort.py
+++ Python/NestedLoops/insertionSort.py
@@ -1,25 +1,28 @@
-def swap (p1, p2, list1):
-    temp = list1[p1]
-    list1[p1] = list1[p2]
-    list1[p2] = temp
+def swap (p1, p2, listC):
+    temp = listC[p1]
+    listC[p1] = listC[p2]
+    listC[p2] = temp
 
 
-def funInner (lastPos):
-    last = list1[lastPos]
+def funInner (listC, lastPos):
+    last = listC[lastPos]
     
     for i in range ( lastPos-1, -1, -1 ):
-        if list1[i] > last : 
-            swap ( i,  lastPos, list1 )
+        if listC[i] > last : 
+            swap ( i,  lastPos, listC )
             lastPos = i
     
-        print (list1)
+        print (listC)
     print ("")
-
+    return listC
 
 list1 = [2, 5, 1, 3, 4, 10, 12, 7]
+print (list1)
+print ("")
+
 len1 = len(list1)
 
 for i in range (len1-1):
     lastPos = i+1 
-    funInner(lastPos)
+    funInner(list1, lastPos)
 
